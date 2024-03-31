8fe1f6e6178f2c1d0900c6f3449f2a2d07eab895
Andrew Smirnov <smirnov@brick.dev>
2024-03-31 17:55:55 +0300

    Add quickSort

--- node/src/factorial.js
+++ node/src/factorial.js
@@ -1,7 +1,9 @@
 "use strict"
 export function factorial(n) {
-    if (n === 0) {
-      return 1;
+    if (n < 0) { return; }
+    let factorial = 1;
+    for (let i = 2; i <= n; i++) {
+        factorial *= i;
     }
-    return n * factorial(n - 1);
+    return factorial;
 };
