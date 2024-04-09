19672aaed7a11674def5b76e73259ca67fba81ae
andreyS <smirnov.andrew.1999@yandex.ru>
2024-04-09 21:12:38 +0300

    Small fix

--- shared/benchMaxArea.js
+++ shared/benchMaxArea.js
@@ -2,5 +2,5 @@
 import { maxArea } from './maxArea.js'
 import { bench } from './bench.js'
 
-const input = new Array(10000).fill().map(_ => Math.round(Math.random() * 10000))
+const input = new Array(100000).fill().map(_ => Math.round(Math.random() * 100000))
 await bench(maxArea, input)
