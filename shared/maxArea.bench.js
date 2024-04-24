87fd342722da7b25a20c794c023c7e71de240f7d
andreyS <smirnov.andrew.1999@yandex.ru>
2024-04-24 17:28:29 +0300

    Remove useless

--- shared/maxArea.bench.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-import { maxArea } from './maxArea.js'
-import { bench } from './bench.js'
-
-const input = new Array(100000).fill().map(_ => Math.round(Math.random() * 100000))
-await bench(maxArea, input)
